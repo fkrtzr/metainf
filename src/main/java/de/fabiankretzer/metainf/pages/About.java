4551bb56e5b5d93b3c836ebaa63f810ceac58cef
Fabian Kretzer <fskretzer@googlemail.com>
2011-03-05 16:38:10 +0100

    initial

---
+++ src/main/java/de/fabiankretzer/metainf/pages/About.java
@@ -0,0 +1,6 @@
+package de.fabiankretzer.metainf.pages;
+
+public class About
+{
+
+}
---
+++ src/main/java/de/fabiankretzer/metainf/pages/Index.java
@@ -0,0 +1,14 @@
+package de.fabiankretzer.metainf.pages;
+
+import java.util.Date;
+
+/**
+ * Start page of application metainf.
+ */
+public class Index
+{
+	public Date getCurrentTime() 
+	{ 
+		return new Date(); 
+	}
+}
